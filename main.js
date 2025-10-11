d56dd4db5e56fe9cc0374f7142a240f94459bfd2
Riguy29 <riguygb@gmail.com>
2025-10-11 00:15:58 -0500

    Expanded Contact Section with Email and Phone

---
+++ main.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const popoverTriggerList = [].slice.call(
+    document.querySelectorAll('[data-bs-toggle="popover"]')
+  );
+  popoverTriggerList.forEach(function (popoverTriggerEl) {
+    new bootstrap.Popover(popoverTriggerEl);
+  });
+});
