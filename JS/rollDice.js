01285036945d934e2f58d48a2b1fd0d21fe52d06
Riguy29 <riguygb@gmail.com>
2026-01-12 18:07:00 -0600

    Prevent Rolling D20 again before animation finishes

--- JS/rollDice.js
+++ JS/rollDice.js
@@ -2,7 +2,15 @@ const d20Dice = document.getElementById("D20-div");
 const diceRollSound = new Audio("assets/sound_effects/dice_roll.mp3");
 const nat1SoundEffect = new Audio("assets/sound_effects/sadTrombone.mp3");
 const nat20SoundEffect = new Audio("assets/sound_effects/fanfare.mp3");
+
+var isD20Rolling = false;
+d20Dice.addEventListener("click", () => {
+  if (isD20Rolling) return;
+  rollDice(document.getElementById("d20-img"), 20);
+});
+
 async function rollDice(img, max) {
+  isD20Rolling = true;
   const randomRotation = 360 * Math.floor(Math.random() * 4 + 3);
   var animationLength = 0.3 * (randomRotation / 360); // .3s per full rotation
   const diceNumberSpan = d20Dice.querySelector(".dice-number");
@@ -14,21 +22,21 @@ async function rollDice(img, max) {
 
   await waitForDiceRollAnimation(animationLength);
 
+  //Reset rotation
   img.style.transition = "transform 0s";
   img.style.transform = `rotate(0deg)`;
 
-  var roll = Math.floor(Math.random() * max) + 1;
-  diceNumberSpan.textContent = roll;
-  if (roll === 20) {
+  //Reset Flag
+  isD20Rolling = false;
+
+  var rollResult = Math.floor(Math.random() * max) + 1;
+  diceNumberSpan.textContent = rollResult;
+  if (rollResult === 20) {
     nat20SoundEffect.play();
-  } else if (roll === 1) {
+  } else if (rollResult === 1) {
     nat1SoundEffect.play();
   }
-  //TODO: Prevent multiple clicks during animation
 }
 async function waitForDiceRollAnimation(animationLength) {
   return new Promise((resolve) => setTimeout(resolve, animationLength * 1000));
 }
-d20Dice.addEventListener("click", () =>
-  rollDice(document.getElementById("d20-img"), 20)
-);
